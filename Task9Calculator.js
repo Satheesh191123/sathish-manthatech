06e5fb2e5db055ede761dd06025440222d8be336
Satheesh191123 <satheesh.071995@gmail.com>
2024-10-04 09:31:21 +0530

    Add files via upload

---
+++ Task9Calculator.js
@@ -0,0 +1,21 @@
+function add(a,b){
+    return a+b;
+}
+function sub(a,b){
+    return a-b;
+}
+function mul(a,b){
+    return a*b;
+}
+function div(a,b){
+    return a/b;
+}
+function mod(a,b){
+    return a%b;
+}
+console.log(add(10,20));
+console.log(sub(10,20));
+console.log(mul(10,20));
+console.log(div(10,20));
+console.log(mod(10,20));
+
